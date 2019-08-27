fd8632397b6dd41525c2fced487173bf0f879cc3
AryaMiss <arya_l@126.com>
2019-08-27 10:30:30 +0800

    add eureka-demo

---
+++ eureka-demo/serviceB/src/main/java/com/mingyin/service/ServiceBApplication.java
@@ -0,0 +1,15 @@
+package com.mingyin.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBApplication.class, args);
+	}
+
+}
---
+++ eureka-demo/serviceB/src/main/java/com/mingyin/service/ServiceBController.java
@@ -0,0 +1,28 @@
+package com.mingyin.service;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Configuration
+public class ServiceBController {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET)
+	public String greeting(@PathVariable("name") String name) {
+		RestTemplate restTemplate = getRestTemplate();
+		return restTemplate.getForObject("http://ServiceA/sayHello/" + name, String.class);
+	}
+	
+}
